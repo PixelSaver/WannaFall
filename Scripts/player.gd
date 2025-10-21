dba20930b808d2d478c3bc8462a2c3e5ed3f6a71
PixelSaver <176425802+PixelSaver@users.noreply.github.com>
2025-10-21 12:45:32 -0700

    got fps controls working (input and mouse)

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -3,7 +3,12 @@ extends CharacterBody3D
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
+@export var mouse_sensitivity : float = .01
+@onready var head: Node3D = $Head
+@onready var camera: Camera3D = $Head/Camera3D
 
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -11,13 +16,13 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
+	var input_dir := Input.get_vector("left", "right", "up", "down")
+	var direction := (head.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
@@ -26,3 +31,10 @@ func _physics_process(delta: float) -> void:
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		head.rotation.y -= event.relative.x * mouse_sensitivity
+		var rot_y = head.rotation.x - event.relative.y * mouse_sensitivity
+		rot_y = clamp(rot_y, -PI/2., PI/2.)
+		head.rotation.x = rot_y
